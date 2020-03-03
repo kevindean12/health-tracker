2d49a8817c8ed37b3a490a3485435e046a3296f0
Kevin Dean <kadean08@gmail.com>
2020-03-03 13:32:03 -0500

    add Register page

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -15,6 +15,7 @@
                 <div class="navbar-end">
                     <router-link class="navbar-item" to="/" active-class="is-current" exact>Home</router-link>
                     <router-link class="navbar-item" to="/about" active-class="is-current">About</router-link>
+                    <router-link class="navbar-item" to="/register" active-class="is-current">Register</router-link>
                     <span class="navbar-item">
                         <router-link class="button is-dark" to="login">
                             <span>Login</span>
